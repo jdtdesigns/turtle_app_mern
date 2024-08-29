53a5c8686c2b3118eb46eff0d72e3b6dd96acee7
JD Tadlock <jt7903@gmail.com>
2024-08-29 11:16:54 -0400

    ready for deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/turtle_db');
+mongoose.connect(process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/turtle_db');
 
 module.exports = mongoose.connection;
